0664580ac5bae35f35b3d51a7d80d6154aa1b338
EineNachtImMai <titouan.cazaubieilh@enseirb-matmeca.fr>
2026-07-23 19:21:04 +0200

    fix: use release instead of dev build for stability

--- nix/default.nix
+++ nix/default.nix
@@ -1,11 +1,17 @@
 {
   buildDotnetModule,
   dotnetCorePackages,
+  fetchFromGitHub,
 }:
-buildDotnetModule {
+buildDotnetModule rec {
   pname = "octo-fiesta";
   version = "0.10";
-  src = ../.;
+  src = fetchFromGitHub {
+    repo = "octo-fiesta";
+    owner = "V1ck3s";
+    tag = "v${version}";
+    sha256 = "sha256-1DMx+PLK9Lxhf052meovrvgya/8WP7YngQjQqwyxOos=";
+  };
 
   doCheck = true;
 
@@ -15,18 +21,18 @@ buildDotnetModule {
   projectFile = "octo-fiesta.sln";
 
   /*
-    to generate / update this file:
-    - `nix build .#default.fetch-deps`
-    - `./result deps.json`
-    - `rm result`
+  to generate / update this file:
+  - `nix build .#default.fetch-deps`
+  - `./result deps.json`
+  - `rm result`
 
-    alternatively, if this isn't working, you can do it manually:
-    - go to the project's root directory
-    - `dotnet restore --packages tmpDir`
-    - `nuget-to-json out > deps.json`
-    - `rm -r tmpDir`
+  alternatively, if this isn't working, you can do it manually:
+  - go to the project's root directory
+  - `dotnet restore --packages tmpDir`
+  - `nuget-to-json out > deps.json`
+  - `rm -r tmpDir`
 
-    source: https://nixos.org/manual/nixpkgs/unstable/#dotnet
+  source: https://nixos.org/manual/nixpkgs/unstable/#dotnet
   */
   nugetDeps = ../deps.json;
 
